bd59e327bbd22d0652e96c60211d3749138c7120
Mii <41633170+InsaneWaifu@users.noreply.github.com>
2023-01-11 20:25:28 +0000

    do tings

--- buildconfig.mjs
+++ buildconfig.mjs
@@ -1,4 +1,4 @@
 export default {
-    replace: "",
-    shouldReplace: false // To avoid doing any replacement, set this to false
+    replace: "~/AppData/Local/Programs/tetrio-desktop/resources/app.asar",
+    shouldReplace: true // To avoid doing any replacement, set this to false
 }
