f31408e14b89b1e853f509d0d37f174903dc7717
Mii <41633170+InsaneWaifu@users.noreply.github.com>
2023-02-21 13:57:30 +0000

    be a little more accurate with comments

--- hooks/preload/hook.js
+++ hooks/preload/hook.js
@@ -1,15 +1,15 @@
 export default class {
-    shouldCapture() {
-        return ["window"]
-    }
-    
-    run(window) {
-        
-    }
+  shouldCapture() {
+    return ["window"]
+  }
 
-    // Any values the script can access that are specified in the return value of shouldCapture() are passed as constructor arguments
-    constructor() {
+  // Any values the script can access that are specified in the return value of shouldCapture() are passed as run arguments
+  run(window) {
 
-    }
+  }
+
+  constructor() {
+
+  }
 }
 // Do not run mod code here as this script will be imported while generating the code for the asar
